3021510394969f8193da97602e733eaca37a2d4b
hbankole <damilarehammed.hd@gmail.com>
2023-03-09 08:01:40 -0800

    100-text

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,9 +1,2 @@
 #!/usr/bin/env ruby
-while line = gets
-  if line =~ /from:(.*?)] \[to:(.*?)] \[flags:(.*?)]/m
-    sender = $1
-    receiver = $2
-    flags = $3
-    puts "#{sender},#{receiver},#{flags}".join(",")
-  end
-end
+puts ARGV[0].scan(/from:(.*?)\] \[to:(.*?)\] \[flags:(.*?)\]/).join(",")
